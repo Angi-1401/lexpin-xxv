9923857081639abd777b1693ede9bc25ee453fa7
Angélica Rivas <57818532+Angi-1401@users.noreply.github.com>
2025-12-08 21:43:41 -0400

    Add JS Content: LocalStorage

---
+++ js/js/local-storage.js
@@ -0,0 +1,22 @@
+/**
+ * Local Storage Utility Functions
+ *
+ * It's a set of helper functions to interact with the browser's local storage.
+ * This allows for easy saving, retrieving, and deleting of data in local
+ * storage.
+ */
+
+const inputField = document.getElementById("text-to-store");
+const saveButton = document.getElementById("save-to-local-storage");
+const updateButton = document.getElementById("btn-update");
+const displaySpan = document.getElementById("local-storage-value");
+
+saveButton.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const textToStore = inputField.value;
+  localStorage.setItem("textData", textToStore);
+});
+
+const storedText = localStorage.getItem("textData");
+displaySpan.textContent = storedText ? storedText : "No data found.";
